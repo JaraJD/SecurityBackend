d5aaaf7c238695f6ef1bb6592b19a3acaa2bb52c
JuanJaramilloSalazar <salazarjdj@gmail.com>
2022-11-19 00:47:21 -0500

    Actualizacion

--- src/main/java/Backend/Controladores/ControladorUsuario.java
+++ src/main/java/Backend/Controladores/ControladorUsuario.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -106,6 +108,18 @@ public class ControladorUsuario {
             return null;
         }
 
+    }
+
+    @PostMapping("/validar")
+    public Usuario validate(@RequestBody Usuario infoUsuario, final HttpServletResponse response) throws IOException {
+        Usuario usuarioActual=this.miRepositorioUsuario.getUserByEmail(infoUsuario.getCorreo());
+        if (usuarioActual!=null && usuarioActual.getContrasena().equals(convertirSHA256(infoUsuario.getContrasena()))) {
+            usuarioActual.setContrasena("");
+            return usuarioActual;
+        }else{
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return null;
+        }
     }
 
 }
